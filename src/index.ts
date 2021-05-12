040ca9e675a73525d756d307c28186b2c2f24319
Pedro Matheus <pedromatheusduarte@gmail.com>
2021-05-12 11:36:56 -0300

    Documentaçao em andamento

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-import express from "express";
 
+import express from "express";
+import swaggerUI from "swagger-ui-express";
 import { usersRoutes } from "./routes/users.routes";
+
+import swaggerfile from './swagger.json'
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerfile))
 
 app.use("/users", usersRoutes);
 
